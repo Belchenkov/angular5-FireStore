a0545aeba60ffa71f536bea2dc6c0e0d24843d50
Belchenkov <u608110@gmail.com>
2017-11-14 11:11:02 +0300

    Finish

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Angular 5 & FireStore Application
 
 > Create an Angular 5 application and integrate AngularFire2 to work with FireStore and fetch items
+
+
+1. git clone
+2. cd directory
+3. npm install
+4. ng serve -o
+
+or go to http://138.197.11.25/ang5ToFirebase/
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAwqVrxkSr6pppW37O0r_rdH_bFeqvFIJA",
+    authDomain: "ang5fire2.firebaseapp.com",
+    databaseURL: "https://ang5fire2.firebaseio.com",
+    projectId: "ang5fire2",
+    storageBucket: "ang5fire2.appspot.com",
+    messagingSenderId: "450499261532"
+  }
 };
